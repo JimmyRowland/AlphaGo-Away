bd1a1c4f2eef2e89d9aa5c4dbf4d22f78e511299
Jimmy Rowland <tian_yu@student.smc.edu>
2021-03-06 21:20:21 -0800

    get_tile_entity_at_location

--- src/core/registry.cpp
+++ src/core/registry.cpp
@@ -6,11 +6,9 @@ entt::entity get_tile_entity_at_position(vec2 position) {
     auto tile_entities = m_registry.view<Tile>();
     int x = floor(position.x / tile_size.x);
     int y = floor(position.y / tile_size.y);
-//    int index = tile_matrix_dimension.x*y+x;
     int index = x * ((tile_matrix_dimension.y - tile_matrix_dimension.x * tile_matrix_dimension.y) /
                      (tile_matrix_dimension.x - 1))
-                + y * ((tile_matrix_dimension.x * tile_matrix_dimension.y + 1) / (tile_matrix_dimension.y - 1) -
-                       (+ tile_matrix_dimension.x * tile_matrix_dimension.y - 1))
+                - y
                 + tile_matrix_dimension.x * tile_matrix_dimension.y - 1;
     return tile_entities[index];
 };
